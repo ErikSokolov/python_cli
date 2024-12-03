8cf0f3d785e66b89b58d54220b99c05012f4da65
Erik <erikstankevich@yandex.com>
2024-12-03 12:12:34 +0100

    Work on fixing mistake

--- cookietyper.py
+++ cookietyper.py
@@ -4,13 +4,15 @@ def main():
         maxx = 10
 
 
-        while i != maxx-1:
+        while i != maxx:
             user_input = input("c?")
-            if user_input == "c":
-                i += 1
-                print(f" you have pressed {i} cookie(s)")
-        else:
-            print(f"You have pressed all {maxx} cookies")
+            while i != maxx-1:
+                if user_input == "c":
+                    i += 1
+                    print(f" you have pressed {i} cookie(s)")
+            if i == maxx:
+                if user_input == "c":
+                    print(f"You have pressed all {maxx} cookies")
             
     foo()
 if __name__ == "__main__":
